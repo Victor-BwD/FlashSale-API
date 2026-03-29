8eb2450998570f4eb3066f6a6a6ea8cf6356cf3d
Victor B. Dornelles <victor.bogdanowdornelles@gmail.com>
2026-03-29 20:17:50 -0300

    feat: Remove unused import in Cucumber test runner

--- infrastructure/src/test/java/infrastructure/bdd/CucumberTestRunner.java
+++ infrastructure/src/test/java/infrastructure/bdd/CucumberTestRunner.java
@@ -2,7 +2,6 @@ package infrastructure.bdd;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
